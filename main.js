d830de6bf48a8e0da0ceb4568804b21538e7a04f
BigFishInSea <154049904+BigFishInSee@users.noreply.github.com>
2026-01-06 20:06:38 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 
-//Skypack broke right before submission. thanks skypack!
-//import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
-//import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
-//import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
 import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.129.0/build/three.module.js";
 import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.129.0/examples/jsm/controls/OrbitControls.js";
 import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
 
+//Skypack broke right before submission. thanks skypack!
+//import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
+//import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
+//import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
 
 
 let controlsTargetPos = null;
@@ -366,6 +366,7 @@ document.addEventListener("mousemove", (e) => {
 
 //Start the 3D rendering
 animate();
+
 
 
 
