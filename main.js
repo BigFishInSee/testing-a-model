7a6ffb8dc6c2833084c2e2d00d2421baf2e263c1
BigFishInSea <154049904+BigFishInSee@users.noreply.github.com>
2026-01-07 13:22:21 +0400

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let controlsTargetPos = null;
 
 let panelOpen = false;
 
-const modelToLoad = "Untitled"
+const modelToLoad = "test"
 
 const parts = {};
 const presetCameras = {};
@@ -102,14 +102,14 @@ const CAMERA_LERP_SPEED = 0.09;
 
 
 
-let objToRender = 'Untitled';
+let objToRender = 'test';
 
 const loader = new GLTFLoader();
 const loadingScreen = document.getElementById("loadingScreen");
 const loadingBar = document.getElementById("loadingBar");
 
 loader.load(
-  'Untitled.glb',
+  'test.glb',
 
   //N LOAD
   function (gltf) {
@@ -210,7 +210,7 @@ renderer.outputColorSpace = THREE.SRGBColorSpace;
 
 document.getElementById("container3D").appendChild(renderer.domElement);
 
-camera.position.z = objToRender === "Untitled" ? 25 : 500;
+camera.position.z = objToRender === "test" ? 25 : 500;
 
 const topLight = new THREE.DirectionalLight(0xffffff, 1); 
 topLight.position.set(500, 500, 500) 
@@ -224,7 +224,7 @@ scene.add(ambientLight);
 
 
 
-if (objToRender === "Untitled") {
+if (objToRender === "test") {
   controls = new OrbitControls(camera, renderer.domElement);
 }
 
@@ -237,7 +237,7 @@ function animate() {
   
   requestAnimationFrame(animate);
 
-  if (object && objToRender === "Untitled") {
+  if (object && objToRender === "test") {
     object.rotation.y = -3 + mouseX / window.innerWidth * 3;
     object.rotation.x = -1.2 + mouseY * 2.5 / window.innerHeight;
   }
@@ -361,6 +361,7 @@ document.addEventListener("mousemove", (e) => {
 
 //Start the 3D rendering
 animate();
+
 
 
 
